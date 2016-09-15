552544c5cb859fe38ae74f8f7f90c02f03a79f3c
Eugeniu-Cuzmin <eugeniu_cuzmin@mail.ru>
2016-09-15 21:10:08 +0300

    login + registration

--- src/main/java/com/spring/template/controller/HomeController.java
+++ src/main/java/com/spring/template/controller/HomeController.java
@@ -3,43 +3,77 @@ package com.spring.template.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.spring.template.model.User;
+import com.spring.template.service.SecurityService;
 import com.spring.template.service.UserService;
+import com.spring.template.validator.UserValidator;
 
 @Controller
 public class HomeController {
-	
+
 	@Autowired
 	private UserService userService;
-	
-	@RequestMapping(value = {"/", "home"}, method = RequestMethod.GET)
+
+	@Autowired
+	private SecurityService securityService;
+
+	@Autowired
+	private UserValidator userValidator;
+
+	@RequestMapping(value = { "/", "home" }, method = RequestMethod.GET)
 	public String home() {
 		return "home";
 	}
-	
+
 	@RequestMapping(value = "/register", method = RequestMethod.GET)
-	public String register() {
+	public String register(Model model) {
+		model.addAttribute("userForm", new User());
 		return "register";
 	}
-	
-	@RequestMapping(value = "/users", method = RequestMethod.GET)
-	public String listAll(Model model){
-		model.addAttribute("users", userService.getAll());
-		return "users";
+
+	@RequestMapping(value = "/register", method = RequestMethod.POST)
+	public String register(@ModelAttribute("userForm") User userForm,
+			BindingResult bindingResult, Model model) {
+		userValidator.validate(userForm, bindingResult);
+
+		if (bindingResult.hasErrors()) {
+			return "registration";
+		}
+
+		userService.save(userForm);
+
+		securityService.autologin(userForm.getUsername(),
+				userForm.getPasswordConfirm());
+		return "redirect:/success";
 	}
-	
-	@RequestMapping(value = "addUser", method = RequestMethod.POST)
-	public String addUser(@ModelAttribute User user){
-		userService.addUser(user);
-		return "redirect:/success.html";
+
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public String login(Model model, String error, String logout) {
+		if (error != null)
+			model.addAttribute("error",
+					"Your username and password is invalid.");
+
+		if (logout != null)
+			model.addAttribute("message",
+					"You have been logged out successfully.");
+
+		return "login";
 	}
-	
-	@RequestMapping(value = "success", method = RequestMethod.GET)
-	public String success(){
+
+	// @RequestMapping(value = "/users", method = RequestMethod.GET)
+	// public String listAll(Model model){
+	// model.addAttribute("users", userService.getAll());
+	// return "users";
+	// }
+	//
+
+	@RequestMapping(value = {"/", "/success"}, method = RequestMethod.GET)
+	public String success() {
 		return "success";
 	}
 }
--- src/main/java/com/spring/template/model/User.java
+++ src/main/java/com/spring/template/model/User.java
@@ -1,52 +1,68 @@
 package com.spring.template.model;
 
+import java.util.Set;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 import javax.persistence.Transient;
 
 @Entity
 @Table(name = "user")
 public class User {
-	@Id
-	@GeneratedValue
-	private int id;
-	private String name;
-	private String password;
-	private String passwordConfirm;
+    private Long id;
+    private String username;
+    private String password;
+    private String passwordConfirm;
+    private Set<Role> roles;
 
-	public int getId() {
-		return id;
-	}
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public Long getId() {
+        return id;
+    }
 
-	public void setId(int id) {
-		this.id = id;
-	}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getUsername() {
+        return username;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setUsername(String username) {
+        this.username = username;
+    }
 
-	public String getPassword() {
-		return password;
-	}
+    public String getPassword() {
+        return password;
+    }
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
-	@Transient
-	public String getPasswordConfirm() {
-		return passwordConfirm;
-	}
+    @Transient
+    public String getPasswordConfirm() {
+        return passwordConfirm;
+    }
 
-	public void setPasswordConfirm(String passwordConfirm) {
-		this.passwordConfirm = passwordConfirm;
-	}
+    public void setPasswordConfirm(String passwordConfirm) {
+        this.passwordConfirm = passwordConfirm;
+    }
 
+    @ManyToMany
+    @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
 }
